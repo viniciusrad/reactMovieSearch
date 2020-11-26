ce5d68c77b9bf6aebf8517c7d2d6b736adee290f
viniciusrad <36489527+viniciusrad@users.noreply.github.com>
2020-11-26 10:37:25 -0300

    busca vazia apaga lista anterior

--- src/searchMovies.js
+++ src/searchMovies.js
@@ -12,7 +12,7 @@ export default function SearchMovies() {
     e.preventDefault();
 
     const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=pt-BR&query=${query}&page=1&include_adult=false`;
-    if (query != "") {
+    if (query !== "") {
       try {
         const res = await fetch(url);
         const data = await res.json();
@@ -23,6 +23,7 @@ export default function SearchMovies() {
         //console.error(err);
       }
     } else {
+      setMovies([]);
       setMsgBuscaVazia(true);
     }
   };
