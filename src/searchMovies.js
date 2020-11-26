94f61961c06d9b93e8927310d2e56a220108f285
viniciusrad <36489527+viniciusrad@users.noreply.github.com>
2020-11-26 08:16:19 -0300

    ajuste na linguagem

--- src/searchMovies.js
+++ src/searchMovies.js
@@ -9,12 +9,13 @@ export default function SearchMovies() {
   const searchMovies = async (e) => {
     e.preventDefault();
 
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=br-PT&query=${query}&page=1&include_adult=false`;
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=pt-BR&query=${query}&page=1&include_adult=false`;
 
     try {
       const res = await fetch(url);
       const data = await res.json();
       setMovies(data.results);
+      console.log(data.results);
     } catch (err) {
       console.error(err);
     }
