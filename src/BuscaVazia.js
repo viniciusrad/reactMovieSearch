66ebfb22d1ece5d68112eb66c56b666b378e3a8f
viniciusrad <36489527+viniciusrad@users.noreply.github.com>
2020-11-26 09:30:04 -0300

    mensagem de compo vazio

---
+++ src/BuscaVazia.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function BuscaVazia(){
+    return(
+        <>
+            <div className="card">
+                <h3>A busca não pode ser vazia</h3>
+            </div>
+        </>
+    )
+}
--- src/searchMovies.js
+++ src/searchMovies.js
@@ -1,23 +1,29 @@
 import React, { useState } from "react";
+import BuscaVazia from "./BuscaVazia.js";
 
 export default function SearchMovies() {
   //states-> input query, movies
   const [query, setQuery] = useState("");
-  //Cria o estado e atualiza o estado
   const [movies, setMovies] = useState([]);
+
+  const [msgBuscaVazia, setMsgBuscaVazia] = useState(false);
 
   const searchMovies = async (e) => {
     e.preventDefault();
 
     const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=pt-BR&query=${query}&page=1&include_adult=false`;
-
-    try {
-      const res = await fetch(url);
-      const data = await res.json();
-      setMovies(data.results);
-      console.log(data.results);
-    } catch (err) {
-      console.error(err);
+    if (query != "") {
+      try {
+        const res = await fetch(url);
+        const data = await res.json();
+        setMovies(data.results);
+        console.log(data.results);
+        setMsgBuscaVazia(false);
+      } catch (err) {
+        //console.error(err);
+      }
+    } else {
+      setMsgBuscaVazia(true);
     }
   };
 
@@ -62,6 +68,8 @@ export default function SearchMovies() {
             </div>
           ))}
       </div>
+      {msgBuscaVazia ? <BuscaVazia /> : null}
+
       <p className="info">
         Desenvolvido por Vinicius Ribeiro, utilizando react e a api fetch do
         javascript
