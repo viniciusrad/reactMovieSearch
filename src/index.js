f625d79c0f5cfa961df035d69794bc8d5a9871cc
viniciusrad <36489527+viniciusrad@users.noreply.github.com>
2020-11-26 08:36:23 -0300

    mudança no titulo

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ class Main extends React.Component {
         return ( 
             <div className = "container" >
            
-            <h1 className = "title" > Busca de filmes com React < /h1> 
+            <h1 className = "title" > Buscador de filmes< /h1> 
             <SearchMovies / >
             </div>
         );
