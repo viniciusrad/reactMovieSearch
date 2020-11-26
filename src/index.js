fec8625d440525c730f189c989d5de83afb68569
viniciusrad <36489527+viniciusrad@users.noreply.github.com>
2020-11-26 06:28:07 -0300

    ultima versão valida

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SearchMovies from "./searchMovies";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Main extends React.Component {
+    render() {
+        return ( <
+            div className = "container" >
+            <
+            h1 className = "title" > Busca de filmes, React < /h1>   <
+            SearchMovies / >
+            <
+            /div>
+        );
+    }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render( < Main / > , document.getElementById('root'));
